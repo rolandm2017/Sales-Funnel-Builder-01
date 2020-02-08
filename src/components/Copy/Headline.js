908a65a22b6c634a037a1f96f1354f6d57074895
plutownium <rolandmackintosh@gmail.com>
2020-02-08 15:08:43 -0800

    added styling to input tags

--- src/components/Copy/Headline.js
+++ src/components/Copy/Headline.js
@@ -2,17 +2,33 @@ import React from "react";
 
 import { connect } from "react-redux";
 
+import styled from "styled-components";
+
+const Div = styled.div`
+	background-color: #b8bec5;
+	margin: 0.5em;
+	padding: 0.5em;
+`;
+
 const headline = props => {
 	return (
 		<div>
-			{props.customText ? <h1>{props.customText}</h1> : <h1>Headline</h1>}
-			{props.pg === 1 ? (
-				<div>
-					<button onClick={props.moveUp}>Move Section Up</button>
-					<button onClick={props.moveDown}>Move Section Down</button>
-					<button onClick={props.del}>Delete Section</button>
-				</div>
-			) : null}
+			<Div>
+				{props.customText ? (
+					<h1>{props.customText}</h1>
+				) : (
+					<h1>Headline</h1>
+				)}
+				{props.pg === 1 ? (
+					<div>
+						<button onClick={props.moveUp}>Move Section Up</button>
+						<button onClick={props.moveDown}>
+							Move Section Down
+						</button>
+						<button onClick={props.del}>Delete Section</button>
+					</div>
+				) : null}
+			</Div>
 		</div>
 	);
 };
--- src/components/Copy/TextArea.js
+++ src/components/Copy/TextArea.js
@@ -2,27 +2,39 @@ import React, { Component } from "react";
 
 import { connect } from "react-redux";
 
+import styled from "styled-components";
+
+const Div = styled.div`
+	background-color: #b8bec5;
+	margin: 0.5em;
+	padding: 0.5em;
+`;
+
 class TextArea extends Component {
 	render() {
 		return (
 			<div>
-				{this.props.customText ? (
-					this.props.customText
-				) : (
-					<p>Text Area: Your Copy Goes Here</p>
-				)}
+				<Div>
+					{this.props.customText ? (
+						this.props.customText
+					) : (
+						<p>Text Area: Your Copy Goes Here</p>
+					)}
 
-				{this.props.pg === 1 ? (
-					<div>
-						<button onClick={this.props.moveUp}>
-							Move Section Up
-						</button>
-						<button onClick={this.props.moveDown}>
-							Move Section Down
-						</button>
-						<button onClick={this.props.del}>Delete Section</button>
-					</div>
-				) : null}
+					{this.props.pg === 1 ? (
+						<div>
+							<button onClick={this.props.moveUp}>
+								Move Section Up
+							</button>
+							<button onClick={this.props.moveDown}>
+								Move Section Down
+							</button>
+							<button onClick={this.props.del}>
+								Delete Section
+							</button>
+						</div>
+					) : null}
+				</Div>
 			</div>
 		);
 	}
